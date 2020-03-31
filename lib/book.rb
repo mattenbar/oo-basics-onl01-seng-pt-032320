141e1ef18c489192bed834c93deccf3968bc467b
matthew enbar <matthew.enbar@gmail.com>
2020-03-31 21:55:01 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,6 +5,10 @@ class Book
     @title = title
   end
 
-  attr_accessor :title, :author, :page_count, :genre 
+  attr_accessor :title, :author, :page_count, :genre
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
 end

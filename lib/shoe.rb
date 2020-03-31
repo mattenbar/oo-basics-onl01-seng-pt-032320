6b69b370d9510cbfa20ecbc21bb62fabf4308be1
matthew enbar <matthew.enbar@gmail.com>
2020-03-31 22:14:15 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,12 +3,15 @@ class Shoe
 
 #getters and setters for shoe class
 attr_accessor :color, :size, :material, :condition
+# since brand never changes we only need a getter not a setter
 attr_reader :brand
 
+#initiales with the brand of the shoe
   def initialize (brand)
     @brand = brand
   end
 
+# a method that fixes shoes and returns their condition to new 
   def cobble
     puts "Your shoe is as good as new!"
     @condition = "new"

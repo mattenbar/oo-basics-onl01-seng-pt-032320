91ea6141ce2731ceeff0b408096f222fe58e8a8d
matthew enbar <matthew.enbar@gmail.com>
2020-03-31 22:14:22 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+# create new class called Book
+class Book
 
+#initializes with a title
+  def initialize (title)
+    @title = title
+  end
 
+#writes the getter and setter for our methods
+  attr_accessor :title, :author, :page_count, :genre
+
+#turn page method has its own output so we write it seperate
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
